d6860ba46598addd58cc958fd4f18b5f38e818ac
Aniket-Bane <aniketbaneani@gmail.com>
2024-04-23 16:23:12 +0530

    added redicre file

---
+++ redirect.js
@@ -0,0 +1,16 @@
+
+
+function redirectByMobileOS() {
+    console.log("Here im from to go to the mobile app")
+    // var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+    // // For Android
+    // if (/android/i.test(userAgent)) {
+    //     window.location.href = "[^1^][1]";
+    // }
+
+    // // For iPhone / iPad / iPod (iOS)
+    // if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+    //     window.location.href = "[^2^][2]";
+    // }
+}
